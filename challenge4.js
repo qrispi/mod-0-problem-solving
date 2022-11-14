5836b41dc7f2781d98b38ee6687698473ccc62a5
Christopher Baum <christopherabaum@gmail.com>
2022-11-14 15:42:37 -0600

    Add directions for each challenge

---
+++ challenge4.js
@@ -0,0 +1,2 @@
+// Start with an array of strings. Print only the words that include the letter combination "ing".
+
---
+++ challenge6.js
@@ -0,0 +1,1 @@
+// Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
