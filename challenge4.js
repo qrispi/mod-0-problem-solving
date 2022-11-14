6b89a129145cabdf1d0380562ab42a7c300b62af
Christopher Baum <christopherabaum@gmail.com>
2022-11-14 15:55:38 -0600

    Solve challenge4

--- challenge4.js
+++ challenge4.js
@@ -7,3 +7,19 @@
 // 2. Find a suitable method for matching a pattern in a string - most likely .includes()
 // 3. Run a for loop that checks each value in the array and prints only the ones that have ing
 
+var array = ["string", "jumping", "guitar", "helping", "whoops", "winged", "hinge"]
+
+// version 1 - shows that .includes only returns true or false, not the element value
+
+// for (var i = 0; i < array.length; i++) {
+//     console.log(array[i].includes("ing"))
+// }
+
+
+// version 2 adds an if statement to then print the actual string values of the array that match our condition
+
+for (var i = 0; i < array.length; i++) {
+    if (array[i].includes("ing")) {
+        console.log(array[i])
+    }
+}
